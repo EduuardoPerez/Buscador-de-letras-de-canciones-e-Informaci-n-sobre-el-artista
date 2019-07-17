2e98ae05063f7002f0a5d309b782bcc84eed4ff9
Eduardo Perez <edujpp1@gmail.com>
2019-07-17 16:29:49 -0300

    Se creo el componente Formulario que de momento maqueta el formulario a través del cual el usuario ingresará la canción y el artista

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, {useState, useEffect, Fragment} from 'react';
+import Formulario from './componentes/Formulario'
 
 function App() {
+
+  // Utilizar useState con 3 states diferentes
+  const[artista, agregarArtista] = useState('');
+  const[letra, agregarLetra] = useState([]);
+  const[info, agregarInfo] = useState({});
+
   return (
-    <p>Hola</p>
+    <Fragment>
+      <Formulario />
+    </Fragment>
   );
 }
 export default App;
